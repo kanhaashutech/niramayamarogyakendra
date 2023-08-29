803fdb3de4b47237336093f1eea29915d455c37d
kanhaashutech <143323206+kanhaashutech@users.noreply.github.com>
2023-08-29 11:57:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -42,12 +42,12 @@ function myFunction(){
     // var x = document.getElementById("menu3");
     // var x = document.getElementById("menu4");
 
-    if(x.style.display == "block"){
-        x.style.display = "none";
+    if(x.style.display == "none"){
+        x.style.display = "block";
 
     }
 
     else{
-        x.style.display = "block";
+        x.style.display = "none";
     }
 }
